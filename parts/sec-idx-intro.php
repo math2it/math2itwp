d5f94b61bae6f8039938e89ed0059fd7cd62367b
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-11-27 18:28:04 +0100

    general layout idx header

--- parts/sec-idx-intro.php
+++ parts/sec-idx-intro.php
@@ -6,16 +6,51 @@
           <img alt="Thi's avatar" src="<?php echo get_bloginfo('template_directory'); ?>/img/math2it.gif">
         </div>
         <h1><?php echo get_bloginfo( 'name' ); ?></h1>
-        <p>Chào mừng các bạn đến với trang web về Toán và Tin học.</p>
+        <h4>Website Toán học & Công nghệ</h4>
+        <p>Chào mừng các bạn đến với trang web về Toán và Tin học. Chào mừng các bạn đến với trang web về Toán và Tin học</p>
         <p class="idx-social">
-          <a class="mr-s" href=""><i class="fa fa-heart"></i> About</a>
-          <a class="mr-s" target="_blank" href="/files/thi_mycv.pdf"><i class="fa fa-user" aria-hidden="true"></i> CV</a>
+          <a class="mr-s" href=""><i class="fa fa-heart"></i> Giới thiệu</a>
+          <a class="mr-s" target="_blank" href="/files/thi_mycv.pdf"><i class="fa fa-user" aria-hidden="true"></i> Math2IT Group</a>
+          <a class="mr-s" target="_blank" href="/files/thi_mycv.pdf"><i class="fa fa-user" aria-hidden="true"></i> Facebook</a>
         </p>
       </div>
-      <div class="col-md-5">
-        <p>
-          Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem Thu nghiem
-        </p>
+      <div class="newest-list col-md-5">
+        <h3 class="new-title">Mới nhất</h3>
+        <div class="new-item">
+          <div class="cat-icon"><i class="icon-tex"></i></div>
+          <div class="new-post">
+            <div class="post-date">22-11-18</div>
+            <h4 class="post-title">Thủ thuật hay cho người viết luận văn và sách bằng LaTeX</h4>
+          </div>
+        </div>
+        <div class="new-item">
+          <div class="cat-icon"><i class="icon-cog-alt"></i></div>
+          <div class="new-post">
+            <div class="post-date">01-02-17</div>
+            <h4 class="post-title">Cách làm trang web ghi chú đơn giản</h4>
+          </div>
+        </div>
+        <div class="new-item">
+          <div class="cat-icon"><i class="icon-language"></i></div>
+          <div class="new-post">
+            <div class="post-date">05-12-07</div>
+            <h4 class="post-title">Làm sao tập trung vào ước mơ của chính mình?</h4>
+          </div>
+        </div>
+        <div class="new-item">
+          <div class="cat-icon"><i class="icon-pi-outline"></i></div>
+          <div class="new-post">
+            <div class="post-date">23-10-06</div>
+            <h4 class="post-title">Lược sử hình thành khái niệm giới hạn trong Toán học</h4>
+          </div>
+        </div>
+        <div class="new-item">
+          <div class="cat-icon"><i class="icon-tex"></i></div>
+          <div class="new-post">
+            <div class="post-date">11-11-11</div>
+            <h4 class="post-title">Thủ thuật hay cho người viết luận văn và sách bằng LaTeX</h4>
+          </div>
+        </div>
       </div>
     </div>
   </div>
