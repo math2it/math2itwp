08ed5c8859f0c366f3198d9e98a68c44aa24ce82
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-12-02 22:58:00 +0100

    add color for cat

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
       /* nav element color depends on color defined in wp admin */
       /* cf. http://bit.ly/2KAagM9 */
       <?php
+        // hover color nav item
         $menuLocations = get_nav_menu_locations();
         $menuID = $menuLocations['math2it-custom-menu'];
         $primaryNav = wp_get_nav_menu_items($menuID);
@@ -24,6 +25,17 @@
           $nav_icon = get_field('nav-icon', $navItem);
           $nav_color = get_field('nav-color', $navItem);
           echo 'a:hover .'.$nav_icon.'{color:'.$nav_color.';}';
+        }
+
+        // hover color for cat
+        $list_categories = get_categories( array(
+            'orderby' => 'name',
+            'order'   => 'ASC'
+        ) );
+        foreach ($list_categories as $cat_item){
+          $cat_icon = get_field('cat-icon', $cat_item);
+          $cat_color = get_field('cat-color', $cat_item);
+          echo 'a:hover .'.$cat_icon.'{color:'.$cat_color.';}';
         }
       ?>
     </style>
