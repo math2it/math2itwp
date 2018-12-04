dc23e77dabbe2803f68ce5519e27d8147c3fb8fc
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-12-04 14:58:35 +0100

    done bacisc layout-1-4

--- parts/sec-idx-math.php
+++ parts/sec-idx-math.php
@@ -24,46 +24,54 @@
           <span class="author">Dinh Anh Thi</span>
         </div>
         <div class="post-intro">
-          Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. 
+          Toán học muôn hình vạn trạng. Lược sử hình thành khái niệm giới hạn trong Toán học. Toán học muôn hình vạn trạng. Toán học muôn hình vạn trạng. Toán học là gì và như thế nào? Toán học muôn hình vạn trạng. 
         </div>
       </div>
 
       <div class="col-md-6">
-        <div class="row">
-          <div class="item style-1-4 col-md-6">
-            <div class="post-title">
-              Lược sử hình thành khái niệm giới hạn trong Toán học
-            </div>
-            <div class="post-meta">
-              <span class="date">21-01-18</span>
-              <span class="author">Nguyen Van A</span>
-            </div>
-          </div>
-          <div class="item style-1-4 col-md-6">
-            <div class="post-title">
-              Toán học là gì và như thế nào?
-            </div>
-            <div class="post-meta">
-              <span class="date">02-11-18</span>
-              <span class="author">Tran Thi B</span>
+        <div class="row style-1-4">
+          <div class="col-md-6 py-md-1">
+            <div class="item">
+              <div class="post-title">
+                Lược sử hình thành khái niệm giới hạn trong Toán học
+              </div>
+              <div class="post-meta">
+                <span class="date">21-01-18</span>
+                <span class="author">Nguyen Van A</span>
+              </div>
             </div>
           </div>
-          <div class="item style-1-4 col-md-6">
-            <div class="post-title">
-              Lược sử hình thành khái niệm giới hạn trong Toán học
-            </div>
-            <div class="post-meta">
-              <span class="date">21-01-18</span>
-              <span class="author">Nguyen Van A</span>
+          <div class="col-md-6 py-md-1">
+            <div class="item">
+              <div class="post-title">
+                Toán học là gì và như thế nào?
+              </div>
+              <div class="post-meta">
+                <span class="date">02-11-18</span>
+                <span class="author">Tran Thi B</span>
+              </div>
             </div>
           </div>
-          <div class="item style-1-4 col-md-6">
-            <div class="post-title">
-              Toán học là gì và như thế nào?
+          <div class="col-md-6 py-md-1">
+            <div class="item">
+              <div class="post-title">
+                Lược sử hình thành khái niệm giới hạn trong Toán học
+              </div>
+              <div class="post-meta">
+                <span class="date">21-01-18</span>
+                <span class="author">Nguyen Van A</span>
+              </div>
             </div>
-            <div class="post-meta">
-              <span class="date">02-11-18</span>
-              <span class="author">Tran Thi B</span>
+          </div>
+          <div class="col-md-6 py-md-1">
+            <div class="item">
+              <div class="post-title">
+                Toán học là gì và như thế nào?
+              </div>
+              <div class="post-meta">
+                <span class="date">02-11-18</span>
+                <span class="author">Tran Thi B</span>
+              </div>
             </div>
           </div>
         </div>
