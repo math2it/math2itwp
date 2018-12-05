71a7f04cc75e2563b6c824320c8b4c07a5368184
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-12-05 11:31:41 +0100

    layout-1-4: icon badge

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>Math2ITwp Theme designed by <a href="https://dinhanhthi.com">Dinh Anh Thi</a>.</p>
       <p>
         <a href="#">Back to top</a>
       </p>
