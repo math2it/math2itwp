104b6dda01f15acfa6a493f932e7d3d47ef64ffd
Anh-Thi DINH <dinhanhthi@gmail.com>
2019-02-14 16:36:03 +0100

    add category outsite each item

--- parts/cat-layout-photo-title.php
+++ parts/cat-layout-photo-title.php
@@ -18,6 +18,13 @@
       <?php foreach($list_posts as $post) : ?>
       <div class="col-12 col-sm-6 col-lg-3">
         <div class="item">
+        <?php
+          $first_cat = get_the_category($post->ID);
+          $rand_number = rand(0,count($first_cat)-1);
+        ?>
+        <?php if ($display_category): ?>
+        <div class="thumb-cat" style="background: <?php echo get_field('dark_color', $first_cat[$rand_number]); ?>;">
+        <?php endif; ?>
           <a class="no-a-effect" href="<?php echo get_permalink($post->ID) ?>">
             <div class="post-image">
               <?php
@@ -32,6 +39,14 @@
               ?>
             </div>
           </a>
+          <?php if ($display_category): ?>
+            <a class="no-a-effect" href="<?php echo esc_url( get_category_link( $first_cat[$rand_number]->term_id ) ) ?>">
+              <div class="post-cat">
+                  <?php echo esc_html( $first_cat[$rand_number]->name ); ?>
+              </div>
+            </a>
+          </div> <!-- /div thumbnail + cat -->
+          <?php endif; ?>
           <div class="post-title">
             <a class="no-a-effect" href="<?php echo get_permalink($post->ID) ?>">
               <?php echo $post->post_title; ?>
