df338952004cdd28223108b863360ffae47358eb
Anh-Thi DINH <dinhanhthi@gmail.com>
2019-02-15 11:55:53 +0100

    update photo-behind

--- parts/cat-layout-photo-behind.php
+++ parts/cat-layout-photo-behind.php
@@ -1,43 +1,34 @@
-<?php 
-  $cat_id = get_query_var('cat_id'); // get category
-
-  $cat_post_args = array(
-    'category'         => $cat_id,
-    'numberposts' 		 => 4,
-  );
-  $cat_posts = get_posts($cat_post_args);
-?>
-
-<?php if ( $cat_posts ) {?>
+<?php if ( $list_posts ) {?>
 
 <section class="layout-photo-behind sec-choice">
 
   <div class="container">
-    <div class="row row-eq-height justify-content-center">
-      
-      <div class="col-12">
-        <div class="sec-title sec-title-small">
-          <h2 class="new-title">
-            <i class="icon-star-circled"></i>
-            Tuyển chọn
-          </h2>
-          <a href="<?php echo get_category_link($cat_id) ?>" class="view-all">xem thêm</a>
-        </div>
-      </div>
 
-      <?php foreach($cat_posts as $post) : ?>
+    <div class="row justify-content-center">
+      <?php if ($typeTitle){get_template_part( 'parts/sec-title' );} ?>
+    </div>
+
+    <div class="row justify-content-center">
+
+      <?php foreach($list_posts as $post) : ?>
       <div class="col-12 col-sm-6 col-md-3">
         <a class="no-a-effect" href="<?php echo get_permalink($post->ID) ?>">
+
+        <?php 
+          $first_cat = get_the_category($post->ID);
+          $rand_number = rand(0,count($first_cat)-1);
+        ?>
+
         <?php 
           if ( has_post_thumbnail($post->ID) ) {
             $postThumbnail = get_the_post_thumbnail_url($post->ID,'large' );
           }else{
             $first_cat = get_the_category($post->ID);
-            $postThumbnail = get_field('default_posts_feature_image','category_'.$cat_id);
+            $postThumbnail = get_field('default_posts_feature_image',$first_cat[$rand_number]);
             $postThumbnail =  wp_get_attachment_url( $postThumbnail['id'],'medium');
           }
         ?>
-        <div class="item d-flex align-items-center" style="background-image: url(<?php echo $postThumbnail ?>)">
+        <div class="item d-flex align-items-end" style="background-image: url(<?php echo $postThumbnail ?>)">
           <div class="item-content">
             <div class="book-title">
               <?php echo $post->post_title; ?>
