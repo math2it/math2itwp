546855af1a34e238b813c81ced1abf5018c3bf2b
Anh-Thi DINH <dinhanhthi@gmail.com>
2019-02-17 21:33:18 +0100

    add link /all

--- parts/sec-idx-intro.php
+++ parts/sec-idx-intro.php
@@ -18,7 +18,7 @@
       <div class="newest-list col-md-5">
         <div class="latest-top sec-title">
           <h3 class="new-title">Mới nhất</h3>
-          <a href="" class="view-all">xem thêm</a>
+          <a href="/all" class="view-all">xem thêm</a>
         </div>
 
         <?php
