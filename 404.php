2e28ed12ba7d11eb3cf562c4a15405bd0200d5d7
Anh-Thi DINH <dinhanhthi@gmail.com>
2019-02-19 22:28:35 +0100

    change bg

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="div-404">
+<div class="div-404 bg-black">
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-12">
@@ -11,7 +11,7 @@
       </div>  
     </div>
     <div class="col-12 col-md-10 col-lg-8">
-      <h2 class="title-404">Không tìm thấy trang</h2>
+      <h2 class="title-404">Không tìm thấy trang!</h2>
       <p>
       Math2IT vừa trải qua đợt cập nhật lớn về nội dung lẫn kỹ thuật. Có thể bài viết bạn cần vẫn còn nhưng địa chỉ đã thay đổi. <strong>Vui lòng dùng chức năng tìm kiếm để tìm lại bài ấy.</strong> Cảm ơn bạn và xin lỗi về sự bất tiện này!
       </p>
