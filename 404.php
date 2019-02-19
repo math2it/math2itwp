c6dd789524e7b58fa1bb8af84027d01b4be427de
Anh-Thi DINH <dinhanhthi@gmail.com>
2019-02-19 12:07:42 +0100

    add 404 page

--- 404.php
+++ 404.php
@@ -1,13 +1,23 @@
 <?php get_header(); ?>
 
+<div class="div-404">
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-12">
-      <div class="text-center py-5">
-        Cannot find what you are looking for.
+      <div class="icon-404">
+        <span>4</span>
+        <i class="icon-math2it"></i>
+        <span>4</span>
       </div>  
     </div>
+    <div class="col-12 col-md-10 col-lg-8">
+      <h2 class="title-404">Không tìm thấy trang</h2>
+      <p>
+      Math2IT vừa trải qua đợt cập nhật lớn về nội dung lẫn kỹ thuật. Có thể bài viết bạn cần vẫn còn nhưng địa chỉ đã thay đổi. <strong>Vui lòng dùng chức năng tìm kiếm để tìm lại bài ấy.</strong> Cảm ơn bạn và xin lỗi về sự bất tiện này!
+      </p>
+    </div>
   </div>
+</div>
 </div>
 
 <?php get_footer(); ?>
