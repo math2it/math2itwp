78ab53106a1fdd7ce42434da77e677d3fc6fd3d3
Anh-Thi DINH <dinhanhthi@gmail.com>
2019-02-26 11:25:40 +0100

    change to awesome icon

--- parts/nav.php
+++ parts/nav.php
@@ -1,12 +1,11 @@
 <nav class="navbar nav-bg navbar-custom">
   <div class="container">
-    <?php header_nav(); ?> <!-- in functions.php -->
+    <?php header_nav(); ?> <!-- in functions-files/nav.php -->
     <div class="nav-search">
       <form action="/" method="get">
-        <input name="s" class="mr-sm-2 nav-search-input" type="search" placeholder="tìm bài viết..." aria-label="tìm bài viết...">
+        <input name="s" class="mr-2 nav-search-input" type="search" placeholder="tìm bài viết..." aria-label="tìm bài viết...">
         <button class="nav-search-submit" type="submit">
-          <!-- <i class="fa fa-search" aria-hidden="true"></i> -->
-          <i class="icon-search"></i>
+          <i class="fa fa-search" aria-hidden="true"></i>
         </button>
       </form>
     </div>
