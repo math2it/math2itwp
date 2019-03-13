d1a9f20042aa79e8db4f907f59315948028c5c12
Dinh Anh Thi <dinhanhthi@gmail.com>
2019-03-13 23:25:58 +0100

    misc

--- plugins/custom-gutenberg-button/my-custom-format.php
+++ plugins/custom-gutenberg-button/my-custom-format.php
@@ -14,6 +14,6 @@ add_action( 'init', 'my_custom_format_script_register' );
 
 function my_custom_format_enqueue_assets_editor() {
 	wp_enqueue_script( 'my-custom-format-js',
-  get_template_directory_uri() . '/plugins/custom-gutenberg-button/my-custom-format.js');
+	get_template_directory_uri() . '/plugins/custom-gutenberg-button/my-custom-format.js');
 }
 add_action( 'enqueue_block_editor_assets', 'my_custom_format_enqueue_assets_editor' );
