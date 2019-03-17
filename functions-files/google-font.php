eb8337700901d6be180ff3dc1e69766e4d91f474
Dinh Anh Thi <dinhanhthi@gmail.com>
2019-03-17 22:41:03 +0100

    fix ssl problem

--- functions-files/google-font.php
+++ functions-files/google-font.php
@@ -4,9 +4,9 @@
 // Add Google Fonts
 // -------------------------------------------------
 function math2itwp_google_fonts() {
-  wp_register_style('Comfortaa', 'http://fonts.googleapis.com/css?family=Comfortaa:400,700&amp;subset=vietnamese');
+  wp_register_style('Comfortaa', 'https://fonts.googleapis.com/css?family=Comfortaa:400,700&amp;subset=vietnamese');
   wp_enqueue_style( 'Comfortaa'); // default font
-  wp_register_style('Righteous', 'http://fonts.googleapis.com/css?family=Righteous'); // math2it's index title
+  wp_register_style('Righteous', 'https://fonts.googleapis.com/css?family=Righteous'); // math2it's index title
   wp_enqueue_style( 'Righteous');
   wp_register_style('Nunito', 'https://fonts.googleapis.com/css?family=Nunito:400,400i,700,700i&amp;subset=vietnamese'); // math2it's index title
   wp_enqueue_style( 'Nunito');
