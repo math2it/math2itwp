274ff04d9331bcc406641281df7f27bdcb43281b
Dinh Anh Thi <dinhanhthi@gmail.com>
2019-03-17 22:42:47 +0100

    add site url

--- parts/nav.php
+++ parts/nav.php
@@ -2,7 +2,7 @@
   <div class="container">
     <?php header_nav(); ?> <!-- in functions-files/nav.php -->
     <div class="nav-search">
-      <form action="/" method="get">
+      <form action="<?php echo get_site_url();?>/" method="get">
         <input name="s" class="mr-2 nav-search-input" type="search" placeholder="tìm bài viết..." aria-label="tìm bài viết...">
         <button class="nav-search-submit" type="submit">
           <i class="fa fa-search" aria-hidden="true"></i>
