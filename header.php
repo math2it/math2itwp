6a9b65c038cf3af9dfe950c885cac4d9ffa08bf1
Dinh Anh Thi <dinhanhthi@gmail.com>
2019-03-24 15:56:30 +0100

    add site url to customURL

--- header.php
+++ header.php
@@ -4,13 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <meta name="description" content="Math2IT's Wordpress Theme">
-    <meta name="author" content="Anh-Thi Dinh">
+    <meta name="description" content="Toán học - Giáo dục - Công nghệ">
+    <meta name="author" content="Math2IT">
     <meta name="google-site-verification" content="181CmgqR5igp35LIcLcdHY9IedJEluRir0meZpAp0rg" />
 
-    <!-- favicon -->
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.gif" />
 
     <title>
@@ -127,7 +125,6 @@
 
   <header>
 
-    <!-- navigation -->
     <?php get_template_part( 'parts/nav' ); ?>
 
   </header>
