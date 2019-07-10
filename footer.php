ccc18d7c4046c534dc41d3040ea1707b42a958e1
Dinh Anh Thi <dinhanhthi@gmail.com>
2019-07-10 14:29:54 +0200

    add newsletter

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
     <footer class="footer">
       <div>
-        <a href="https://fb.com/math2it" target="_blank">Facebook</a> | 
-        <a href="https://fb.com/groups/math2it" target="_blank">Math2IT Group</a> | 
+        <a href="https://fb.com/math2it" target="_blank">Facebook</a> |
+        <a href="https://fb.com/groups/math2it" target="_blank">Group</a> |
         <a href="<?php echo get_site_url() ?>/thanks">Cảm ơn</a> |
+        <a href="<?php echo get_site_url() ?>/newsletter">Newsletter</a>
+         |
         <a href="<?php echo get_site_url() ?>/privacy">Quyền riêng tư</a>
       </div>
       <div id="up-arrow">
